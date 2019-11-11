2be2ef79ea91ee4e6268b62d44a1c2413b96864e
Francis Gramaglia <francis.gramaglia@yahoo.fr>
2019-11-11 18:50:45 +0100

    Ajout projet sous asp.Net core 2.2

---
+++ Firebird.AspNetCore22-MVC-Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Firebird.AspNetCore22_MVC_Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
