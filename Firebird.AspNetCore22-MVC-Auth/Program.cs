411dec0a74bd9e1a791031ced6adbb097bc8e9d1
Francis Gramaglia <francis.gramaglia@yahoo.fr>
2019-11-12 10:57:17 +0100

    Ajout Nlog

--- Firebird.AspNetCore22-MVC-Auth/Program.cs
+++ Firebird.AspNetCore22-MVC-Auth/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace Firebird.AspNetCore22_MVC_Auth
 {
@@ -17,8 +18,29 @@ namespace Firebird.AspNetCore22_MVC_Auth
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            IWebHostBuilder webhostbuilder = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    //logging.ClearProviders();
+                })
+                .UseNLog();  // NLog: setup NLog for Dependency injection
+
+
+
+            webhostbuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                var rootDirectory = env.ContentRootPath;
+                AppDomain.CurrentDomain.SetData("RootDirectory", rootDirectory);
+                string absolute = Path.GetFullPath(Path.Combine(rootDirectory, "App_Data"));
+                if (!Directory.Exists(absolute))
+                    Directory.CreateDirectory(absolute);
+                AppDomain.CurrentDomain.SetData("DataDirectory", absolute);
+            });
+            return webhostbuilder;
+        }
     }
 }
